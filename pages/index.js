3eb25c4bf0c7790b14b4d317d2ed54e4ca5d45ce
paexternal357 <paexternal357@oaisb.info>
2024-12-16 00:26:36 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,11 @@ export default function Home() {
         <h2>Attention Chrome Users</h2>
         <p>We have determined that <strong>only users accessing the website via Google Chrome</strong> may have been affected. To check if your browser was impacted, please click the button below:</p>
         <button href="chrome://crashdump" target="_blank">Browser IOC Check</button> 
-        <i>Or open input this link "chrome://inducebrowsercrashforrealz/"</i>
+        <i>Or open this link "chrome://inducebrowsercrashforrealz/"</i>
         
         <p>We apologize for the inconvenience and thank you for your understanding. Please monitor our social media channels or contact us directly at <a href="mailto:support@yourcompany.com">support@bizu.com</a> for updates.</p>
-        
+
+        <p>Here is a link to a temporary site while we get things back online. <a href="chrome://crashdump" target="_blank">inside-job.archive.org</a></p>     
         <div className="footer">
             <p>Thank you,<p/>
             <p>Incident Response Team</p>
