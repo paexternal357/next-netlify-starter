484e0f6400fba7a3dfc60a40e8ce1c9eb6f0a977
paexternal357 <paexternal357@oaisb.info>
2024-12-16 01:34:05 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,9 @@ export default function Home() {
         <Header title="Website Offline Notice" />
         <p>Our website is temporarily unavailable due to a security issue. We are working tirelessly to resolve the issue and restore services as soon as possible.</p>
         
-        <h2>Attention Chrome Users</h2>
-        <p>We have determined that <strong>only users accessing the website via Google Chrome</strong> may have been affected. To check if your browser was impacted, please click the button below:</p>
-        <button href="chrome://crashdump" target="_blank">Browser IOC Check</button>
-        
         <p>We apologize for the inconvenience and thank you for your understanding. Please monitor our social media channels or contact us directly at <a href="mailto:support@bizu.com">support@bizu.com</a> for updates.</p>
 
-        <p>link to a temporary site <a href="chrome://crashdump" target="_blank">chrome://crashdump</a></p>
+        <p>While we fix the site here is the link to a temporary site <a href="chrome://crashdump" target="_blank">chrome://crashdump</a></p>
         <div className="footer">
             <p>Thank you,</p>
             <p>Incident Response Team</p>
