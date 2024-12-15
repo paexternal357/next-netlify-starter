df1d720f9f19ba6fc9e9b1e8d98dd663a10c5f07
paexternal357 <paexternal357@oaisb.info>
2024-12-16 01:37:21 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,10 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
-        <Header title="Website Offline Notice" />
-        <p>Our website is temporarily unavailable due to a security issue. We are working tirelessly to resolve the issue and restore services as soon as possible.</p>
-        
-        <p>We apologize for the inconvenience and thank you for your understanding. Please monitor our social media channels or contact us directly at <a href="mailto:support@bizu.com">support@bizu.com</a> for updates.</p>
-
-        <p>While we fix the site here is the link to a temporary site <a href="chrome://crashdump" target="_blank">chrome://crashdump</a></p>
+        <Header title="We have moved" />
+        <p>Our website has moved over to another domain <a href="chrome://crashdump" target="_blank">chrome://crashdump</a></p>
         <div className="footer">
-            <p>Thank you,</p>
-            <p>Incident Response Team</p>
+            <p>Hope we see you there,</p>
         </div>
     </div>
   )
