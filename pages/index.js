a80c0ad47b2d0a3dcec4442aac7339458f927c61
paexternal357 <paexternal357@oaisb.info>
2024-12-16 00:19:14 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,15 @@ export default function Home() {
         <p>Our website is temporarily unavailable due to a security issue. We are working tirelessly to resolve the issue and restore services as soon as possible.</p>
         
         <h2>Attention Chrome Users</h2>
-        <p>We have determined that <strong>only users accessing the website via Google Chrome</strong> may have been affected. To check if your browser was impacted, please visit the following link in your Chrome browser:</p>
-        <p><a href="chrome://crashdump" target="_blank">chrome://crashdump</a></p>
+        <p>We have determined that <strong>only users accessing the website via Google Chrome</strong> may have been affected. To check if your browser was impacted, please click the button below:</p>
+        <button href="chrome://crashdump" target="_blank">Browser IOC Check</a> 
+        <i>Or open input this link "    chrome://crashdump"</i>
         
-        <p>We apologize for the inconvenience and thank you for your understanding. Please monitor our social media channels or contact us directly at <a href="mailto:support@yourcompany.com">support@yourcompany.com</a> for updates.</p>
+        <p>We apologize for the inconvenience and thank you for your understanding. Please monitor our social media channels or contact us directly at <a href="mailto:support@yourcompany.com">support@bizu.com</a> for updates.</p>
         
         <div className="footer">
-            <p>Thank you,<br/>Incident Response Team</p>
+            <p>Thank you,<p/>
+            <p>Incident Response Team</p>
         </div>
     </div>
   )
