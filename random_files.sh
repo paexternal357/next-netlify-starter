8b9a1d05cab1c05c81b949bdec6ea47955e4fbd7
paexternal357 <paexternal357@oaisb.info>
2025-01-29 06:45:14 -0800

    6

--- random_files.sh
+++ random_files.sh
@@ -66,7 +66,7 @@ tell application "Finder"
     
     set screenWidth to 1920 -- Adjust for your screen resolution
     set screenHeight to 1080 -- Adjust for your screen resolution
-
+    
     repeat with i from 1 to count desktopItems
         set itemPosition to {random number from 0 to (screenWidth - 100), random number from 0 to (screenHeight - 100)}
         set position of item i of desktopItems to itemPosition
