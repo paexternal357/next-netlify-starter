6e4576713e95e7bac1be90bdbfec672230fda64d
paexternal357 <paexternal357@oaisb.info>
2025-02-10 13:20:43 -0800

    splitter-final

--- split-manual.sh
+++ split-manual.sh
@@ -1,14 +1,14 @@
 #!/bin/env sh
 
-chunk_size=$((16 * 1024 * 1048576)); #gigs in megabytes
-input_file="macOS 13 Ventura VMDK by techrechard.com.vmdk"
+chunk_size=$((14 * 1024 * 1048576)); #gigs in megabytes
+input_file="2"
 
 len=`stat -f'%z' "$input_file"`
 chunks=$(($len/$chunk_size + 1))
 
 for i in `seq 0 $chunks`
 do
-  dd if="$input_file" skip=$i of="$input_file.part.$i" count=1 bs=$chunk_size
+  dd if="$input_file" skip=$i of="$input_file.part.$i" count=1 bs=$chunk_size status=progress status=progress
   read -n1 -s
   # scp $input_file.part servername:path/$input_file.part.$i
 done
