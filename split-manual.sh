7eea5fad4d8144bd38bd66f68160aee60927085c
paexternal357 <paexternal357@oaisb.info>
2025-02-10 13:26:38 -0800

    splitter

--- split-manual.sh
+++ split-manual.sh
@@ -8,7 +8,7 @@ chunks=$(($len/$chunk_size + 1))
 
 for i in `seq 0 $chunks`
 do
-  dd if="$input_file" skip=$i of="$input_file.part.$i" count=1 bs=$chunk_size status=progress status=progress
+  dd if="$input_file" skip=$i of="$input_file.part.$i" count=1 bs=$chunk_size status=progress
   read -n1 -s
   # scp $input_file.part servername:path/$input_file.part.$i
 done
