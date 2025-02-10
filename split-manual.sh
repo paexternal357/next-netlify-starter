4ced1b0a1d2cf981ace5d46c749da5acefeb569f
paexternal357 <paexternal357@oaisb.info>
2025-02-10 12:49:17 -0800

    splitter

--- split-manual.sh
+++ split-manual.sh
@@ -1,12 +1,13 @@
 #!/bin/env sh
 
-chunk_size = 2048 * 14336; #gigs in megabytes
-input_file = "macOS 13 Ventura VMDK by techrechard.com.vmdk"
+chunk_size=$((16 * 1024 * 1048576)); #gigs in megabytes
+input_file="macOS 13 Ventura VMDK by techrechard.com.vmdk"
 
-len = `stat '%s' $input_file`
-chunks = $(($len/$chunk_size + 1))
+len=`stat -c'%s' "$input_file"`
+chunks=$(($len/$chunk_size + 1))
+echo $chunks
 
-for i in {0...$chunks}
+for i in `seq 0 $chunks`
 do
   dd if=$input_file skip=$i of=$input_file.part.$i count=1 bs=$chunk_size
   read -n1 -s
