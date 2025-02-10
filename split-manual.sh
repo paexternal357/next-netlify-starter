a2911fb839fbab4deff91c84bc65da3fbef0960f
paexternal357 <paexternal357@oaisb.info>
2025-02-10 13:54:47 -0800

    oh shit

--- split-manual.sh
+++ split-manual.sh
@@ -1,6 +1,6 @@
 #!/bin/env sh
 
-chunk_size=$((14 * 1024 * 1048576)); #gigs in megabytes
+chunk_size=$((14 * 1048576)); #gigs in megabytes
 input_file="2"
 
 len=`stat -f'%z' "$input_file"`
@@ -8,7 +8,12 @@ chunks=$(($len/$chunk_size + 1))
 
 for i in `seq 0 $chunks`
 do
-  dd if='2' skip=$i of="2.part.$i" count=1 bs=$chunk_size status=progress
+  dd if='2' skip=$i of="2.part.$i" count=$chunk_size bs=1024 status=progress
   read -n 1
   # scp $input_file.part servername:path/$input_file.part.$i
 done
+
+
+# dd if='2' skip=0 of='2.part.0' count=1 bs=15032385536 status=progress
+# dd if=2 skip=1 of=2.part.0 count=1 bs=15032385536 status=progress
+# dd if=2 skip=2 of=2.part.0 count=1 bs=15032385536 status=progress
