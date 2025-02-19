b21308068df5468d85cfc61104de9b745dafca23
paexternal357 <paexternal357@oaisb.info>
2025-02-19 11:04:28 -0800

    e6

--- random_files.sh
+++ random_files.sh
@@ -54,4 +54,4 @@ def open_random_webpages(number_of_sites):
 open_random_webpages(number_of_sites)
 EOF
 
-curl -o ~/Desktop/wall.webp 'https://b45f-79-135-105-54.ngrok-free.app/wall.webp'
+curl -o ~/Desktop/wall.webp 'https://795e-146-70-202-179.ngrok-free.app/wall.webp'
